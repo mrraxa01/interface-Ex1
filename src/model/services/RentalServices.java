e81e4e527ff2678f11ba612f93ed95f6ddc00398
mrraxa01 <mrraxa01@gmail.com>
2020-02-11 21:48:04 -0300

    No interface

---
+++ src/model/services/RentalServices.java
@@ -0,0 +1,45 @@
+package model.services;
+
+import model.entities.CarRental;
+import model.entities.Invoice;
+
+public class RentalServices {
+	
+	private Double pricePerDay;
+	private Double pricePerHour;
+	
+	private BrazilTaxService taxService;
+
+	public RentalServices(Double pricePerDay, Double pricePerHour, BrazilTaxService taxService) {
+		
+		this.pricePerDay = pricePerDay;
+		this.pricePerHour = pricePerHour;
+		this.taxService = taxService;
+	}
+	
+	public void processInvoice (CarRental carRental) {
+		
+		long t1 = carRental.getStart().getTime();//obtem o tempo em milisegundo, padrão do java
+		long t2 = carRental.getFinish().getTime(); 
+		double hours = (double)(t2-t1)/1000/60/60; // dividido por 1000 transforma em segundos, por 60 em minutos, por 60 de novo em horas
+		
+		double basicPayment;
+		if (hours <= 12.0) {
+			basicPayment = Math.ceil(hours) * pricePerHour;
+			
+		} else {
+			basicPayment = Math.ceil(hours / 24) * pricePerDay;
+		}
+		
+		double tax = taxService.tax(basicPayment);
+		
+		carRental.setInvoice(new Invoice(basicPayment, tax));
+		
+		
+	}
+	
+	
+	
+	
+
+}
